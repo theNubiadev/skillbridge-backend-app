7d3e0ce9796e8677bf932291d1d60c85f844d30b
theNubiadev <ajaiyeobajibola@gmail.com>
2025-10-02 22:47:37 +0100

    i now connected the user table to the client table and jobs to scale and reduce db size

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,45 +1,48 @@
-import jwt from "jsonwebtoken"
-import userModel from "../models/userModels.js"
-import clientModel from "../models/clientProfileModel.js"
+import jwt from "jsonwebtoken";
+import userModel from "../models/userModels.js";
+import clientModel from "../models/clientProfileModel.js";
 
-const authMiddleware = async (req, res, next) => { 
-    const { token } = req.headers;
-    //  Bearer token
+const authMiddleware = async (req, res, next) => {
+  const { token } = req.headers;
 
-    //  if token is not provided 
-    if (!token) {
-        return res.status(401).json({
-            success: false, 
-            message: "No token provided"
-        })
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "No token provided",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // ✅ fetch user
+    const user = await userModel.findById(decoded.id);
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found",
+      });
     }
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = await userModel.findById(decoded.id);
-            //  
-        if (!req.user) {
-            return res.status(401).json({
-                success: false,
-                message: "User not found"
-            })
-        }
 
-        // req.user = user;
-    // If role is client, fetch client profile and attach its _id
-    // if (req.user.role === "client") {
-    //   const clientProfile = await clientModel.findOne({ user: user._id });
-    //   if (clientProfile) {
-    //     req.user.clientProfileId = clientProfile._id;
-    //   }
-    // }
-        next();
-    } catch (error) {
-        console.log(error);
-        return res.status(401).json({
-            success: false, 
-            message: "Invalid token"
-        })
+    // attach to req
+    req.user = user;
+
+    // ✅ If role is client, fetch client profile and attach its _id
+    if (user.role === "client") {
+      const clientProfile = await clientModel.findOne({ user: user._id });
+      if (clientProfile) {
+        req.user.clientProfileId = clientProfile._id;
+      }
     }
-}
+
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({
+      success: false,
+      message: "Invalid token",
+    });
+  }
+};
 
 export default authMiddleware;
